08450f9310281bf43cd9c304e042a81609756b7a
JaSON <masteraccounthub@gmail.com>
2021-04-08 06:16:18 -0400

    commit tree ac60e8b6b580fbf6d5d73f1a4de244acef185f89

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    return { ...driver, ...{ [key]: value } }
+
+  }
+
+
+  function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value;
+
+    return driver;
+
+  }
+
+
+  function deleteFromDriverByKey(driver, key) {
+    const newO = { ...driver }
+
+  delete newO[key];
+
+  return newO;
+
+  }
+
+  function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key];
+
+    return driver;
+  }
